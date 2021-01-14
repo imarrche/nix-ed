567fdccca5d75306b743b004c0d21d6b75cb7401
Arrche <imarrche@gmail.com>
2021-01-14 11:06:06 +0200

    Add file storage for posts.

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 var n = 100
 
-func fetchPostByID(id int, ch chan string) {
+func fetchPostByID(id int, ch chan []byte) {
 	r, err := http.Get(fmt.Sprintf("https://jsonplaceholder.typicode.com/posts/%d", id))
 	if err != nil {
 		log.Fatalf("couldn't fetch post %d: %v", id, err)
@@ -21,16 +21,33 @@ func fetchPostByID(id int, ch chan string) {
 		log.Fatal(err)
 	}
 
-	ch <- string(body)
+	ch <- body
 }
 
 func main() {
-	ch := make(chan string)
+	ch := make(chan []byte)
 	for id := 1; id <= n; id++ {
 		go fetchPostByID(id, ch)
 	}
 
 	for i := 1; i <= n; i++ {
-		log.Println(<-ch)
+		// ioutil
+		err := ioutil.WriteFile(fmt.Sprintf("./storage/posts/%d.txt", i), <-ch, 0755)
+		if err != nil {
+			log.Fatalf("couldn't write file %d: %v", i, err)
+		}
+
+		// bufio
+		// Can write into anything that implements io.Writer interface.
+		// f, err := os.Create(fmt.Sprintf("./storage/posts/%d.txt", i))
+		// if err != nil {
+		// 	log.Fatalf("couldn't create file %d: %v", i, err)
+		// }
+		// f.Sync()
+		// w := bufio.NewWriter(f)
+		// if _, err := w.Write(<-ch); err != nil {
+		// 	log.Fatalf("couldn't write file %d: %v", i, err)
+		// }
+		// w.Flush()
 	}
 }
