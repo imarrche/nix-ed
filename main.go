0584bd16a869c938397e92e42b168a6f27df5445
Arrche <imarrche@gmail.com>
2021-01-14 10:31:59 +0200

    Add posts fetching.

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func fetchPosts() {
+	r, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		log.Fatalf("couldn't fetch posts: %v", err)
+	}
+	defer r.Body.Close()
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(string(body))
+}
 
 func main() {
-	fmt.Println("Hello, NIX Education!")
+	fetchPosts()
 }
