14f50df7aaa6e76a2a914c2adfe61942a78f7639
Arrche <imarrche@gmail.com>
2021-02-08 14:48:19 +0200

    Switch to Echo

--- internal/comment/handler.go
+++ internal/comment/handler.go
@@ -1,148 +1,103 @@
 package comment
 
 import (
-	"encoding/json"
-	"encoding/xml"
 	"net/http"
 	"strconv"
 
-	"github.com/go-chi/chi"
-	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/labstack/echo"
+
 	"github.com/imarrche/nix-ed/internal/model"
 )
 
-// Handler is http handler for all comment endpoints.
+// Handler is http handler for comment resource.
 type Handler struct {
-	r *chi.Mux
 	s Service
 }
 
-// NewHandler creates and returns a new Handler instance.
+// NewHandler creates and returns a new Handler instacne.
 func NewHandler(s Service) *Handler {
-	return &Handler{chi.NewRouter(), s}
+	return &Handler{s}
 }
 
-// Init initializes all handler's routes.
-func (h *Handler) Init(r chi.Router) {
-	r.Get("/", h.getAll)
-	r.Post("/", h.create)
-	r.Get("/{id:[0-9]+}", h.detail)
-	r.Patch("/{id:[0-9]+}", h.update)
-	r.Delete("/{id:[0-9]+}", h.delete)
-}
-
-// ServeHTTP makes router to handle requests.
-func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.r.ServeHTTP(w, r)
-}
-
-// respond is a helper function for encapsulating responding logic.
-func (h *Handler) respond(w http.ResponseWriter, r *http.Request, status int, data interface{}) {
-	w.WriteHeader(status)
-
-	type encoder interface {
-		Encode(interface{}) error
-	}
-
-	var enc encoder
-	if r.Header.Get("Accept-Encoding") == "application/xml" {
-		w.Header().Set("Content-Type", "application/xml")
-		enc = xml.NewEncoder(w)
-	} else {
-		w.Header().Set("Content-Type", "application/json")
-		enc = json.NewEncoder(w)
-	}
-
-	if e, ok := data.(validation.Errors); ok {
-		enc.Encode(e)
-	} else if e, ok := data.(error); ok {
-		enc.Encode(map[string]interface{}{"error": e.Error()})
-	} else if data != nil {
-		enc.Encode(data)
-	}
-}
-
-// getAll handles retrieving all comments.
-func (h *Handler) getAll(w http.ResponseWriter, r *http.Request) {
+// GetAll returns comment list.
+func (h *Handler) GetAll(c echo.Context) error {
 	cs, err := h.s.GetAll()
 	if err != nil {
-		h.respond(w, r, http.StatusInternalServerError, nil)
-		return
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	h.respond(w, r, http.StatusOK, cs)
+	return c.JSON(http.StatusOK, cs)
+
 }
 
-// create handles comment creating.
-func (h *Handler) create(w http.ResponseWriter, r *http.Request) {
-	c := model.Comment{}
-	if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+// Create creates a comment.
+func (h *Handler) Create(c echo.Context) error {
+	cm := model.Comment{}
+	if err := c.Bind(&cm); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	c, err := h.s.Create(c)
+	cm, err := h.s.Create(cm)
 	if err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	h.respond(w, r, http.StatusCreated, c)
+	return c.JSON(http.StatusCreated, cm)
 }
 
-// detail handles retrieving a single comment.
-func (h *Handler) detail(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+// GetByID returns comment detail.
+func (h *Handler) GetByID(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		h.respond(w, r, http.StatusInternalServerError, nil)
-		return
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	c, err := h.s.GetByID(id)
-	if err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+	cm, err := h.s.GetByID(id)
+	if err == ErrNotFound {
+		return c.NoContent(http.StatusNotFound)
+	} else if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	h.respond(w, r, http.StatusOK, c)
+	return c.JSON(http.StatusCreated, cm)
 }
 
-// update handles updating a comment.
-func (h *Handler) update(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+// Update updates a comment.
+func (h *Handler) Update(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		h.respond(w, r, http.StatusInternalServerError, nil)
-		return
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	c := model.Comment{}
-	if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+	cm := model.Comment{}
+	if err := c.Bind(&cm); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
-	c.ID = id
+	cm.ID = id
 
-	c, err = h.s.Update(c)
-	if err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+	cm, err = h.s.Update(cm)
+	if err == ErrNotFound {
+		return c.NoContent(http.StatusNotFound)
+	} else if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	h.respond(w, r, http.StatusOK, c)
+	return c.JSON(http.StatusCreated, cm)
 }
 
-// delete handles deleting a comment.
-func (h *Handler) delete(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+// DeleteByID deletes a comment.
+func (h *Handler) DeleteByID(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		h.respond(w, r, http.StatusInternalServerError, nil)
-		return
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	if err = h.s.DeleteByID(id); err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+	err = h.s.DeleteByID(id)
+	if err == ErrNotFound {
+		return c.NoContent(http.StatusNotFound)
+	} else if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	h.respond(w, r, http.StatusNoContent, nil)
+	return c.NoContent(http.StatusNoContent)
 }
--- internal/post/handler.go
+++ internal/post/handler.go
@@ -1,148 +1,103 @@
 package post
 
 import (
-	"encoding/json"
-	"encoding/xml"
 	"net/http"
 	"strconv"
 
-	"github.com/go-chi/chi"
-	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/labstack/echo"
+
 	"github.com/imarrche/nix-ed/internal/model"
 )
 
-// Handler is http handler for all post endpoints.
+// Handler is http handler for post resource.
 type Handler struct {
-	r *chi.Mux
 	s Service
 }
 
-// NewHandler creates and returns a new Handler instance.
+// NewHandler creates and returns a new Handler instacne.
 func NewHandler(s Service) *Handler {
-	return &Handler{chi.NewRouter(), s}
+	return &Handler{s}
 }
 
-// Init initializes all handler's routes.
-func (h *Handler) Init(r chi.Router) {
-	r.Get("/", h.getAll)
-	r.Post("/", h.create)
-	r.Get("/{id:[0-9]+}", h.detail)
-	r.Patch("/{id:[0-9]+}", h.update)
-	r.Delete("/{id:[0-9]+}", h.delete)
-}
-
-// ServeHTTP makes router to handle requests.
-func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.r.ServeHTTP(w, r)
-}
-
-// respond is a helper function for encapsulating responding logic.
-func (h *Handler) respond(w http.ResponseWriter, r *http.Request, status int, data interface{}) {
-	w.WriteHeader(status)
-
-	type encoder interface {
-		Encode(interface{}) error
-	}
-
-	var enc encoder
-	if r.Header.Get("Accept-Encoding") == "application/xml" {
-		w.Header().Set("Content-Type", "application/xml")
-		enc = xml.NewEncoder(w)
-	} else {
-		w.Header().Set("Content-Type", "application/json")
-		enc = json.NewEncoder(w)
-	}
-
-	if e, ok := data.(validation.Errors); ok {
-		enc.Encode(e)
-	} else if e, ok := data.(error); ok {
-		enc.Encode(map[string]interface{}{"error": e.Error()})
-	} else if data != nil {
-		enc.Encode(data)
-	}
-}
-
-// getAll handles retrieving all posts.
-func (h *Handler) getAll(w http.ResponseWriter, r *http.Request) {
+// GetAll returns post list.
+func (h *Handler) GetAll(c echo.Context) error {
 	ps, err := h.s.GetAll()
 	if err != nil {
-		h.respond(w, r, http.StatusInternalServerError, nil)
-		return
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	h.respond(w, r, http.StatusOK, ps)
+	return c.JSON(http.StatusOK, ps)
+
 }
 
-// create handles post creating.
-func (h *Handler) create(w http.ResponseWriter, r *http.Request) {
+// Create creates a post.
+func (h *Handler) Create(c echo.Context) error {
 	p := model.Post{}
-	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+	if err := c.Bind(&p); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
 	p, err := h.s.Create(p)
 	if err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	h.respond(w, r, http.StatusCreated, p)
+	return c.JSON(http.StatusCreated, p)
 }
 
-// detail handles retrieving a single post.
-func (h *Handler) detail(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+// GetByID returns post detail.
+func (h *Handler) GetByID(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		h.respond(w, r, http.StatusInternalServerError, nil)
-		return
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
 	p, err := h.s.GetByID(id)
-	if err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+	if err == ErrNotFound {
+		return c.NoContent(http.StatusNotFound)
+	} else if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	h.respond(w, r, http.StatusOK, p)
+	return c.JSON(http.StatusCreated, p)
 }
 
-// update handles updating a post.
-func (h *Handler) update(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+// Update updates a post.
+func (h *Handler) Update(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		h.respond(w, r, http.StatusInternalServerError, nil)
-		return
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
 	p := model.Post{}
-	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+	if err := c.Bind(&p); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
 	p.ID = id
 
 	p, err = h.s.Update(p)
-	if err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+	if err == ErrNotFound {
+		return c.NoContent(http.StatusNotFound)
+	} else if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	h.respond(w, r, http.StatusOK, p)
+	return c.JSON(http.StatusCreated, p)
 }
 
-// delete handles deleting a post.
-func (h *Handler) delete(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+// DeleteByID deletes a post.
+func (h *Handler) DeleteByID(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		h.respond(w, r, http.StatusInternalServerError, nil)
-		return
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	if err = h.s.DeleteByID(id); err != nil {
-		h.respond(w, r, http.StatusBadRequest, err)
-		return
+	err = h.s.DeleteByID(id)
+	if err == ErrNotFound {
+		return c.NoContent(http.StatusNotFound)
+	} else if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	h.respond(w, r, http.StatusNoContent, nil)
+	return c.NoContent(http.StatusNoContent)
 }
